bf9eb65f63794139dff2cdcd75680941da3736ce
rafelps <rafel1995@gmail.com>
2020-12-05 11:34:47 +0100

    Add try/except for server error codes

--- collect_tweets.py
+++ collect_tweets.py
@@ -15,12 +15,13 @@ def local_time_str():
 	return strftime('%H:%M:%S', localtime())
 
 def log(file, query):
-	file.write(strftime('%a, %d %b %Y %H:%M:%S', localtime()) + '\n')
-	file.write('Name = ' + query['name'] + '\n')
-	file.write('Search = ' + query['search'] + '\n')
-	file.write('Lang = ' + str(query['lang']) + '\n')
-	file.write('Since = ' + str(query['since']) + '\n')
-	file.write('Until = ' + str(query['until']) + '\n\n\n')
+	with open(file, 'a') as f:
+		f.write(strftime('%a, %d %b %Y %H:%M:%S', localtime()) + '\n')
+		f.write('Name = ' + query['name'] + '\n')
+		f.write('Search = ' + query['search'] + '\n')
+		f.write('Lang = ' + str(query['lang']) + '\n')
+		f.write('Since = ' + str(query['since']) + '\n')
+		f.write('Until = ' + str(query['until']) + '\n\n\n')
 
 
 def make_query(api, db, query):
@@ -32,20 +33,28 @@ def make_query(api, db, query):
 	since = time()
 	print('Collecting tweets for query \"{}\"...'.format(query['name']))
 
-	for tweet in tweepy.Cursor(api.search, q=query['search'], lang=query['lang'], since=query['since'], until=query['until'], tweet_mode='extended').items():
+	cursor = tweepy.Cursor(api.search, q=query['search'], lang=query['lang'], since=query['since'], until=query['until'], tweet_mode='extended').items()
 
-		json_data = tweet._json
-		json_data['_id'] = json_data['id']
+	while True:
 		try:
-			col.insert_one(json_data)
-		except pymongo.errors.DuplicateKeyError:
-			print('Duplicated detected')
-			break
+			for tweet in cursor:
 
-		c += 1
-		if c % 1000 == 0:
-			t = time() - since
-			print('{} --> Collected {:6d} tweets in {}'.format(local_time_str(), c, wall_time_str(t)))
+				json_data = tweet._json
+				json_data['_id'] = json_data['id']
+				try:
+					col.insert_one(json_data)
+				except pymongo.errors.DuplicateKeyError:
+					print('Duplicated detected')
+					break
+
+				c += 1
+				if c % 1000 == 0:
+					t = time() - since
+					print('{} --> Collected {:6d} tweets in {}'.format(local_time_str(), c, wall_time_str(t)))
+			break
+		except tweepy.error.TweepError as e:
+			print('{} --> {}'.format(local_time_str(), e))
+			continue
 
 	print('Done collecting tweets for query {}.\n\n'.format(query['name']))
 
@@ -78,12 +87,12 @@ if __name__ == '__main__':
 	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 	queries = [
-		{'name': 'vaccine_langs',
-		'search': '#Vaccine OR #Vacuna OR #Vaccin OR #Impfstoffe OR #Vaccino',
-		'lang': None,
-		'since': None,
-		'until': None
-		},
+		# {'name': 'vaccine_langs',
+		# 'search': '#Vaccine OR #Vacuna OR #Vaccin OR #Impfstoffe OR #Vaccino',
+		# 'lang': None,
+		# 'since': None,
+		# 'until': None
+		# },
 		{'name': 'elections2020_single',
 		'search': '#Elections2020',
 		'lang': 'en',
@@ -116,10 +125,8 @@ if __name__ == '__main__':
 		},
 	]
 
-	log_file = open('log.txt', 'a')
+	log_file = 'log.txt'
 
 	for query in queries:
-		make_query(api, db, query)
 		log(log_file, query)
-
-	log_file.close()
+		make_query(api, db, query)
