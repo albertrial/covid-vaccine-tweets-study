717fefd42d1287a0d22d5fc6cf2458060af21cdb
rafel <rafel1995@gmail.com>
2020-12-09 22:04:55 +0100

    Modify make_query() params. Add dict unpacking. Update logs

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,4 +1,3 @@
-# import pprint
 import tweepy
 import pymongo
 from time import time, strftime, localtime, gmtime, sleep
@@ -22,16 +21,16 @@ def log(file, query):
 		f.write('\n\n')
 
 
-def make_query(api, db, query):
+def make_query(api, db, name, search, lang=None, since=None, until=None, max_id=None, items=0):
 
 	# Create Collection
-	col = db[query['name']]
+	col = db[name]
 
 	c = 0
-	since = time()
-	print('Collecting tweets for query \"{}\"...'.format(query['name']))
+	since_runtime = time()
+	print('Collecting tweets for query \"{}\"...'.format(name))
 
-	cursor = tweepy.Cursor(api.search, q=query['search'], lang=query['lang'], since=query['since'], until=query['until'], max_id=query['max_id'], tweet_mode='extended').items(query['items'])
+	cursor = tweepy.Cursor(api.search, q=search, lang=lang, since=since, until=until, max_id=max_id, tweet_mode='extended').items(items)
 
 	while True:
 		try:
@@ -47,7 +46,7 @@ def make_query(api, db, query):
 
 				c += 1
 				if c % 1000 == 0:
-					t = time() - since
+					t = time() - since_runtime
 					print('{} --> Collected {:6d} tweets in {}'.format(local_time_str(), c, wall_time_str(t)))
 			break
 		except tweepy.error.TweepError as e:
@@ -55,25 +54,12 @@ def make_query(api, db, query):
 			sleep(600)
 			continue
 
-	print('Done collecting tweets for query {}.\n\n'.format(query['name']))
+	print('Collected {} tweets in {} for query \"{}\".\n\n'.format(c, wall_time_str(time() - since_runtime), name))
 
 ######################################
 
 # query https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/guides/standard-operators
 # params https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
-
-# + Una amb tots els llenguatges (#vaccine or #vacuna #....)
-# + Un hashtag d'eleccions (Elections2020) a veure què passa
-# + Una provant el filtre positiu de twitter
-# + Una provant el filtre negatiu de twitter
-# + Una query sense hashtag (sino vaccine directament)
-# + Una query amb #COVID19
-# - Una query amb #EndSARS
-
-
-# query = ('Query_Name', 'Query_Search', 'lang', 'since', 'until')
-# query = ('Vaccine_no_since', '#Vaccine', None, None, None)
-
 
 if __name__ == '__main__':
 
@@ -86,21 +72,45 @@ if __name__ == '__main__':
 	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 	queries = [
-		{'name': 'vaccine_top3_hashtags',
-		'search': '#vaccine OR #vacuna OR #vaccin',
-		'lang': None,
-		'since': None,
-		'until': None,
-		'items': 1000000,
-		'max_id': 1333861537669386243
-		},
-		{'name': 'vaccine_top3_words',
-		'search': 'vaccine OR vacuna OR vaccin',
+		# {'name': 'vaccine_top3_hashtags',
+		# 'search': '#vaccine OR #vacuna OR #vaccin',
+		# 'lang': None,
+		# 'since': None,
+		# 'until': None,
+		# 'items': 1000000,
+		# 'max_id': 1333861537669386243
+		# },
+		# {'name': 'vaccine_top3_words',
+		# 'search': 'vaccine OR vacuna OR vaccin',
+		# 'lang': None,
+		# 'since': None,
+		# 'until': None,
+		# 'max_id': 1335349527549767690,
+		# 'items': 100000
+		# },
+		# {'name': 'covid_hashtags_24h',
+		# 'search': '#COVID19 OR #COVID',
+		# 'lang': None,
+		# 'since': '2020-12-05',
+		# 'until': '2020-12-06',
+		# 'max_id': None,
+		# 'items': 1000000
+		# },
+		# {'name': 'covid_vaccine',
+		# 'search': 'covid vaccine OR vacuna OR vaccin',
+		# 'lang': None,
+		# 'since': None,
+		# 'until': None,
+		# 'max_id': None,
+		# 'items': 50000
+		# },
+		{'name': '#covid_vaccine',
+		'search': '#covid OR #covid19 vaccine OR vacuna OR vaccin',
 		'lang': None,
 		'since': None,
 		'until': None,
 		'max_id': None,
-		'items': 150000
+		'items': 0
 		},
 	]
 
@@ -108,4 +118,4 @@ if __name__ == '__main__':
 
 	for query in queries:
 		log(log_file, query)
-		make_query(api, db, query)
+		make_query(api, db, **query)
