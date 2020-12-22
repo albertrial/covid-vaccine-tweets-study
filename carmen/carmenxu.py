73c140f328a14480d7f29ffa86592f0e9bd5f966
rafelps <rafel1995@gmail.com>
2020-12-22 16:55:46 +0100

    Add carmen

---
+++ carmen/carmenxu.py
@@ -0,0 +1,121 @@
+import re
+import json
+import pymongo
+from collections import defaultdict
+
+
+STATE_RE = re.compile(r'(.+),\s*(\w+)')								# Find Structures such Alaior, Menorca
+WHITES_RE = re.compile(r'\s+')										# Find whitespaces, tabs and newlines
+PUNCTUATION_RE = re.compile(r'\W+')									# Find all punctuation signs
+SEPARATORS_RE = re.compile(r'(?:(?: \-)|[/|\\\(\),])+')				# Find "important" separators --> Alaior, Menorca. Alaior - Menorca. Alaior/Menorca. Alaior|Menorca. Alaior\Menorca. Alaior (Menorca)
+STRIPPING_RE = re.compile(r'^\W+|\W+$')								# Find non-alphanumeric characters at the start and end of string
+
+
+def normalize(location_name):
+	x = re.sub(WHITES_RE, ' ', location_name.lower())               # Collapse whitespaces, tabs and newlines into a single whitespaces
+	x = re.sub(PUNCTUATION_RE, ' ', x)                              # Substitute all punctuation signs by whitespaces
+	x = x.strip()                                                   # Remove extra whitespaces
+	
+	return x
+
+
+def normalize_commas(location_name):
+	x = re.sub(WHITES_RE, ' ', location_name.strip().lower())       # Collapse whitespaces, tabs and newlines into a single whitespaces
+	x = re.sub(SEPARATORS_RE, 'a1b2c3d4e5', x)                      # Detect "important" separators, and replace them to a special token
+	x = re.sub(PUNCTUATION_RE, ' ', x)                              # Substitute all the remaining punctuation signs by whitespaces
+	x = re.sub(re.compile(r'\s*a1b2c3d4e5\s*'), ', ', x)            # Convert all the important separators to ', '
+	x = re.sub(STRIPPING_RE, '', x)                                 # Remove extra whitespaces or ', '
+	return x
+
+
+def load_known_locations(filename):
+	with open(filename, encoding='utf-8') as f:
+		location_list = json.load(f)
+
+	alias_location = {}
+	location_dict = defaultdict(lambda: None)
+	for location in location_list:
+		location_dict[location['_id']] = location
+		for alias in location['aliases']:
+			alias_location[alias] = location['_id']
+
+	return alias_location, location_dict
+
+
+def resolve_tweet(tweet, force_coordinates=False):
+	# Resolve by place
+	place = tweet['place']
+	if place:
+		match = match_comma_structure(normalize_commas(place['full_name']))
+		if not match:
+			normalized_country = normalize(place['country'])
+			if normalized_country in ALIAS_LOCATION:
+				match = normalized_country
+		if match:
+			return ensure_coordinates(ALIAS_LOCATION[match], force_coordinates)
+
+	# Resolve by user.location
+	user_location = tweet['user']['location']
+	if user_location:
+		# Normalize without commas, just like the aliases
+		normalized = normalize(user_location)
+
+		# Exact normalized match
+		if normalized in ALIAS_LOCATION:
+			return ensure_coordinates(ALIAS_LOCATION[normalized], force_coordinates)
+
+		# Try preserving commas and matching states
+		match = match_comma_structure(normalize_commas(user_location))
+		if match:
+			return ensure_coordinates(ALIAS_LOCATION[match], force_coordinates)
+	
+	return None
+
+
+def match_comma_structure(normalized_location):
+	match = STATE_RE.search(normalized_location)
+	provisional_match = None
+	while match:
+		before_comma = match.group(1)
+		after_comma = match.group(2)
+		
+		if before_comma in ALIAS_LOCATION:
+			provisional_match = before_comma
+		elif after_comma in ALIAS_LOCATION:
+			provisional_match = after_comma
+
+		match = STATE_RE.search(before_comma)
+	return provisional_match
+
+
+def ensure_coordinates(_id, force_coordinates):
+	if not force_coordinates:
+		return _id
+	while _id != '-1' and 'latitude' not in LOCATION_DICT[_id]:
+		_id = LOCATION_DICT[_id]['parent_id']
+	# Any parent has coordinates
+	if _id == '-1':
+		_id = None
+	return _id
+
+
+######################################################################
+if __name__ == '__main__':
+	client = pymongo.MongoClient('fpsds.synology.me', 27017, username='mongoadmin', password='bda')
+	tweets_col = client['tweets']['#covid_vaccine']
+
+	ALIAS_LOCATION, LOCATION_DICT = load_known_locations('known_locations.json')
+
+	tweets = tweets_col.find({'my_geo': {'$exists': False}}, {'user.location': 1, 'place': 1, 'geo': 1, 'coordinates': 1, '_id': 1})
+	has_coordinates = count = total = 0
+	for tweet in tweets:
+		location = resolve_tweet(tweet, force_coordinates=True)
+		if location:
+			if 'latitude' in LOCATION_DICT[location]:
+				has_coordinates += 1
+			count += 1
+		tweets_col.update_one({'_id': tweet['_id']}, {'$set': {'my_geo': LOCATION_DICT[location]}})
+		total += 1
+
+	print(count, total, 100*count/total)
+	print(has_coordinates, total, 100*has_coordinates/total)
---
+++ carmen/insert_carmen_locations.py
@@ -0,0 +1,115 @@
+import pymongo
+import json
+from carmenxu import normalize, normalize_commas
+from collections import defaultdict
+
+
+def insert_locations():
+	## SET PYMONGO ##
+	client = pymongo.MongoClient('fpsds.synology.me', 27017, username='mongoadmin', password='bda')
+	geo_col = client['geo']['known_locations']
+
+	# Open locations file
+	with open('locations.json', encoding='utf-8') as f:
+		lines = f.readlines()
+	
+	all_aliases = defaultdict(list)
+	all_locations = {}
+
+	for line in lines:
+		location = json.loads(line)
+		location['_id'] = location.pop('id')
+		loc_id = location['_id']
+		all_locations[loc_id] = location
+
+		aliases = []
+		if 'aliases' in location:
+			aliases = location['aliases']
+		
+		processed_aliases = set()
+		# Add normalizations
+		for alias in aliases:
+			if alias not in processed_aliases:
+				processed_aliases.add(alias)
+				processed_aliases.add(normalize(alias))
+				processed_aliases.add(normalize_commas(alias))
+
+		# Add countrycode
+		if location['parent_id'] == '-1' and 'countrycode' in location:
+			processed_aliases.add(location['countrycode'].lower())
+		# Add statecode for USA locations
+		if location['parent_id'] == '2645' and 'statecode' in location:
+			processed_aliases.add(location['statecode'].lower())
+		# Add city, county, state and country
+		if location['city'] != '':
+			processed_aliases.add(location['city'].lower())
+		elif location['county'] != '':
+			processed_aliases.add(location['county'].lower())
+		elif location['state'] != '':
+			processed_aliases.add(location['state'].lower())
+		else:
+			processed_aliases.add(location['country'].lower())
+
+		for alias in processed_aliases:
+			all_aliases[alias].append(loc_id)
+
+	# Duplicate Processing
+	for key in frozenset(all_aliases.keys()):
+		if len(all_aliases[key]) == 1:
+			all_aliases[key] = all_aliases[key][0]
+		else:
+			locs = all_aliases[key]
+			provisional_cities = []
+			provisional_counties = []
+			provisional_states = []
+			provisional_countries = []
+			for loc in locs:
+				if all_locations[loc]['city'] != '':
+					provisional_cities.append(loc)
+				elif all_locations[loc]['county'] != '':
+					provisional_counties.append(loc)
+				elif all_locations[loc]['state'] != '':
+					provisional_states.append(loc)
+				else:
+					provisional_countries.append(loc)
+			if len(provisional_cities) == 1:
+				all_aliases[key] = provisional_cities[0]
+			elif len(provisional_cities) > 1:
+				del all_aliases[key]
+			else:
+				if len(provisional_counties) == 1:
+					all_aliases[key] = provisional_counties[0]
+				elif len(provisional_counties) > 1:
+					del all_aliases[key]
+				else:
+					if len(provisional_states) == 1:
+						all_aliases[key] = provisional_states[0]
+					elif len(provisional_states) > 1:
+						del all_aliases[key]
+					else:
+						if len(provisional_counties) == 1:
+							all_aliases[key] = provisional_counties[0]
+						else:
+							del all_aliases[key]
+
+
+	# Compute final list of aliases for each location
+	location_aliases = defaultdict(list)
+	for alias in all_aliases.keys():
+		location_aliases[all_aliases[alias]].append(alias)
+
+	# Update aliases list and insert to db
+	for loc_id in all_locations:
+		loc = all_locations[loc_id]
+		loc['aliases'] = location_aliases[loc_id]
+		geo_col.insert_one(loc)
+		# try:
+		# 	geo_col.insert_one(location)
+		# except pymongo.errors.DuplicateKeyError:
+		# 	continue
+
+	print('{} locations added to the database'.format(len(location_aliases)))
+
+
+if __name__ == '__main__':
+	insert_locations()
