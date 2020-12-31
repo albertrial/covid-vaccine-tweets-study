3c294c12f2d38c2fdc4b903ac190a7ed3c88566c
rafelps <rafel1995@gmail.com>
2020-12-31 12:52:50 +0100

    Add script to convert full datetime to shorter sortable string

---
+++ src/datetime_convertion.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import pymongo
+from keys import MONGODB_KEY
+
+client = pymongo.MongoClient('localhost', 27017, username='mongoadmin', password=MONGODB_KEY)
+db = client['tweets']
+tweets = db['#covid_vaccine']
+
+for tweet in tweets.find({'date': {'$exists': False}}, {'created_at':1}):
+    # Parse formatted string
+    dt = datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S %z %Y')
+    # Include new field with format YYYY-MM-DD
+    tweets.update_one({'_id': tweet['_id']}, {'$set': {'date': dt.strftime('%Y-%m-%d')}})
